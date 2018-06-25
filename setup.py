76988a4e38c451b2e39c1efc217c46e66c83fe94
Matthew Scholefield <matthew331199@gmail.com>
2018-06-25 16:38:29 -0500

    Increment version to 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='petact',
-    version='0.1.1',
+    version='0.1.2',
     description='A package extraction tool',
     url='https://github.com/matthewscholefield/petact',
     author='Matthew Scholefield',
